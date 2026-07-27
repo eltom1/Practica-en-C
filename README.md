1b8de48cc5d7743b5c68f6590ac1a7ff23e913d2
eltom1 <tomasdellosso4@gmail.com>
2026-07-27 19:57:35 -0300

    ej nuevos

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Practica-en-C
-
-hola
---
+++ practica2.c
@@ -0,0 +1,43 @@
+
+// Practica 2 en c
+
+#include <stdio.h>
+
+//ARREGLOS 
+
+//ejercicio 8
+
+void ej8(){
+    int arreglo[100];
+
+    for(int i=0; i<=100;i++){
+        arreglo[i] = i;
+        printf("%d ", arreglo[i]);
+
+    }
+}
+
+void ej9(){
+int arreglo[51];
+
+    int j=0;
+    for(int i=200;i>=100; i-= 2){
+        arreglo[j]=i;
+        printf("pos[%d]= %d\n", j,arreglo[j]);
+        j++;
+        }
+}
+
+void ej10(){
+    int arreglo[51];
+    for(int i=0; i<=51; i-=3){
+        printf("%d ", arreglo[i]);
+    }
+}
+
+int main(){
+    //ej8();
+    ej9();
+    ej10();
+    return 0;
+}
