b679292655331432aa35cfddeb3f3523162310b1
eltom1 <tomasdellosso4@gmail.com>
2026-07-30 12:13:04 -0300

    ejercicio 2

--- final-hecho29.c
+++ final-hecho29.c
@@ -10,17 +10,39 @@ float horas_promedio(float horario[], int cantidad){
     return sum / cantidad;
 }
 
+int mal_dormidos(float horario[],int cantidad,float promedio,int durmieron_mal[]){
+    int acum = 0;
+    for(int i=0; i<cantidad; i++){
+        if(horario[i]< promedio ){
+            durmieron_mal[acum] = i;
+            acum++;
+        }
+    }
+    return acum;
+    return durmieron_mal[cantidad];
+}
+
+
+
 int main(){
 
     float h_dormir[CANTIDAD] = 
     {6.5, 7.0 , 5.5, 8.0, 6.8, 7.5,
          4.5, 9.1, 5.0, 6.8, 7.3, 9.1};
 
-    printf("%.2f\n", horas_promedio(h_dormir, CANTIDAD));
-    assert(horas_promedio(h_dormir,CANTIDAD)== 6.93);
+    int durmieron_mal[CANTIDAD];
+
+    printf("%.2f\n", horas_promedio(h_dormir, CANTIDAD)); // el prom es 6.93
+
+    // assert(horas_promedio(h_dormir,CANTIDAD)== 6.93);
+
+    float prom = horas_promedio(h_dormir, CANTIDAD);
+
+    printf("%d\n", mal_dormidos(h_dormir, CANTIDAD, prom, durmieron_mal));
+    printf("%d \n", durmieron_mal[CANTIDAD]);
     return 0;
 }
  
 
-
+ 
 
