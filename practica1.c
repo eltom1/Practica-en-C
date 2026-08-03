e78031bdfae57d924ecff9c638a402b256e1df5b
eltom1 <tomasdellosso4@gmail.com>
2026-08-03 16:55:50 -0300

    movi archivos

--- practica1.c
+++ practica1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+
+int ej11(){
+    int i= 0;
+    while(i<100){
+        printf("%d\n", i);
+        i++;
+    }
+}
+
 int main(){
-    printf("hola mundo");
+    ej11();
+
+    printf("hola mundo\n");
     return 0;
 
 }
+
